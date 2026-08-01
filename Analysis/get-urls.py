a1a01f3254fa35cb62d48220038baf82663b5d0d
h7n <silent.h7n@gmail.com>
2026-08-01 08:34:23 +0530

    day 2

--- Analysis/get-urls.py
+++ Analysis/get-urls.py
@@ -20,7 +20,7 @@ class GetAllUrls:
     
     #for creating files for the tools and store there output indivisually   
     def __make__file(self,tool):
-        Path(f"{tool}.txt").write_text("Initial recon notes\n")
+        Path(f"{tool}.txt")
 
     def run_gau(self):
         self.__make__file('_gau_')
@@ -55,6 +55,9 @@ class GetAllUrls:
         count = os.popen('wc -l < _katana_.txt').read().strip()
         print(f'[+] Endpoints found: {count}')
 
+    def run_gospider(self):
+        self.__make__file("_gospider_")
+
     
     def rm_junk(self): #removing duplicates
         self.__make__file('_LiveUrls_.txt')
@@ -64,7 +67,9 @@ class GetAllUrls:
         print(f'[+]Process complete')
         print(f'[+]File saved to _LiveUrls_.txt')
 
-        count = os.popen('wc -l < _gau_.txt').read().strip()
+        os.system(cmd)
+
+        count = os.popen('wc -l < _LiveUrls_.txt').read().strip()
         print(f'[+]Current URls : {count}')
 
 
@@ -85,4 +90,3 @@ class GetAllUrls:
 
 tool = GetAllUrls('example.com')
 tool.run_all()
-
